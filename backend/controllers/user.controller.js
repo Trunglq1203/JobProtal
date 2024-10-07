28dff09509c53774fe83f65eefcfd1155232305f
LeQuangTrung <trunglqhe172533@fpt.edu.vn>
2024-10-07 13:32:35 +0700

    Update code

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,29 +1,22 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import getDataUri from "../utils/datauri.js";
-import cloudinary from "../utils/cloudinary.js";
 
 export const register = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, password, role } = req.body;
-         
         if (!fullname || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
-                message: "Something is missing",
+                message: "Something is missing.",
                 success: false
-            });
+            })
         };
-        const file = req.file;
-        const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
-
         const user = await User.findOne({ email });
         if (user) {
             return res.status(400).json({
-                message: 'User already exist with this email.',
+                message: "User already exists with this email.",
                 success: false,
-            })
+            });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -33,9 +26,6 @@ export const register = async (req, res) => {
             phoneNumber,
             password: hashedPassword,
             role,
-            profile:{
-                profilePhoto:cloudResponse.secure_url,
-            }
         });
 
         return res.status(201).json({
@@ -45,14 +35,13 @@ export const register = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-}
+};
 export const login = async (req, res) => {
     try {
         const { email, password, role } = req.body;
-        
         if (!email || !password || !role) {
             return res.status(400).json({
-                message: "Something is missing",
+                message: "Something is missing.",
                 success: false
             });
         };
@@ -60,18 +49,18 @@ export const login = async (req, res) => {
         if (!user) {
             return res.status(400).json({
                 message: "Incorrect email or password.",
-                success: false,
-            })
+                success: false
+            });
         }
         const isPasswordMatch = await bcrypt.compare(password, user.password);
         if (!isPasswordMatch) {
             return res.status(400).json({
                 message: "Incorrect email or password.",
-                success: false,
+                success: false
             })
         };
-        // check role is correct or not
-        if (role !== user.role) {
+        //check role is correct or not
+        if (user.role !== role) {
             return res.status(400).json({
                 message: "Account doesn't exist with current role.",
                 success: false
@@ -92,11 +81,11 @@ export const login = async (req, res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1000 * 60 * 60 * 24 * 1, httpOnly: true, sameSite: 'strict' }).json({
             message: `Welcome back ${user.fullname}`,
-            user,
             success: true
         })
+
     } catch (error) {
         console.log(error);
     }
@@ -114,44 +103,39 @@ export const logout = async (req, res) => {
 export const updateProfile = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, bio, skills } = req.body;
-        
         const file = req.file;
+        if (!fullname || !email || !phoneNumber || !bio || !skills) {
+            return res.status(400).json({
+                message: "Something is missing.",
+                success: false
+            });
+        };
+
         // cloudinary ayega idhar
-        const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
 
-
-
-        let skillsArray;
-        if(skills){
-            skillsArray = skills.split(",");
-        }
-        const userId = req.id; // middleware authentication
+        const skillsArray = skills.split(",");
+        const userId = req.id; //from middleware authenticator
         let user = await User.findById(userId);
 
         if (!user) {
             return res.status(400).json({
                 message: "User not found.",
                 success: false
-            })
+            });
         }
-        // updating data
-        if(fullname) user.fullname = fullname
-        if(email) user.email = email
-        if(phoneNumber)  user.phoneNumber = phoneNumber
-        if(bio) user.profile.bio = bio
-        if(skills) user.profile.skills = skillsArray
-      
-        // resume comes later here...
-        if(cloudResponse){
-            user.profile.resume = cloudResponse.secure_url // save the cloudinary url
-            user.profile.resumeOriginalName = file.originalname // Save the original file name
-        }
+        //updating data
+        user.fullname = fullname;
+        user.email = email;
+        user.phoneNumber = phoneNumber;
+        user.profile.bio = bio;
+        user.profile.skills = skillsArray;
+
+        //resume comes later here...
 
 
         await user.save();
 
-        user = {
+        uesr = {
             _id: user._id,
             fullname: user.fullname,
             email: user.email,
@@ -161,9 +145,9 @@ export const updateProfile = async (req, res) => {
         }
 
         return res.status(200).json({
-            message:"Profile updated successfully.",
+            message: "Profile updated successfully.",
             user,
-            success:true
+            success: true
         })
     } catch (error) {
         console.log(error);
